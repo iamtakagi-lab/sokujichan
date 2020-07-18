68ff784effb323b6bd45a39f1a44745b2f16b3b8
notsmatch <smatchjp@gmail.com>
2020-07-19 05:55:10 +0900

    fix: guide url

--- src/me/notsmatch/sokujichan/Config.kt
+++ src/me/notsmatch/sokujichan/Config.kt
@@ -5,5 +5,5 @@ import java.awt.Color
 object Config {
 
     val EMBED_COLOR = Color(83, 221, 172)
-    val GUIDE_URL = "https://riptakagi.me/22"
+    val GUIDE_URL = "https://riptakagi.me/sokujichan"
 }
