b3306deaeac53872213ebf982a8d07834987db42
takagi <iam.takagi.dev@gmail.com>
2021-02-10 00:25:18 +0900

    feat: Bot

--- src/main/kotlin/me/takagi/sokujichan/Main.kt
+++ src/main/kotlin/me/takagi/sokujichan/Main.kt
@@ -1,10 +1,14 @@
 package me.takagi.sokujichan
 
+import me.takagi.sokujichan.common.Env
 import io.ktor.application.*
 import io.ktor.server.cio.CIO
 import io.ktor.server.engine.*
+import me.takagi.sokujichan.bot.Bot
 
 fun main(args: Array<String>) {
+    Bot(Env.BOT_TOKEN).start()
+
     embeddedServer(CIO,
         port = Env.PORT,
         host = Env.HOST,
