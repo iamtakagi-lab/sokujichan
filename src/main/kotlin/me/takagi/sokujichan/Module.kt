10b8e4663aae568474864901172d0b06f0466b58
takagi <iam.takagi.dev@gmail.com>
2021-02-10 00:25:41 +0900

    fix: Logging & static 宣言を追加

--- src/main/kotlin/me/takagi/sokujichan/Module.kt
+++ src/main/kotlin/me/takagi/sokujichan/Module.kt
@@ -1,19 +1,48 @@
 package me.takagi.sokujichan
 
 import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.http.*
+import io.ktor.http.content.*
+import io.ktor.request.*
 import io.ktor.routing.*
-import io.ktor.util.KtorExperimentalAPI
+import me.takagi.sokujichan.common.Env
+import me.takagi.sokujichan.common.createLogger
+import me.takagi.sokujichan.endpoints.getIndex
+import me.takagi.sokujichan.endpoints.getOverlay
+import mu.KotlinLogging
 
-
-@KtorExperimentalAPI
 fun Application.module() {
 
-    val bot by lazy {
-        Bot(Env.TOKEN).start()
+    install(CallLogging) {
+        logger = KotlinLogging.createLogger("sokujichan.server")
+        format { call ->
+            when (val status = call.response.status()) {
+                HttpStatusCode.Found -> "$status: ${call.request.toLogString()} -> ${call.response.headers[HttpHeaders.Location]}"
+                null -> ""
+                else -> "$status: ${call.request.httpMethod.value} ${call.request.uri}"
+            }
+        }
     }
 
     routing {
-        getOverlay()
+        route(Env.BASE_URI) {
+
+            getIndex()
+
+            static("static") {
+                static("css") {
+                    resources("css")
+                }
+                static("icon") {
+                    resources("icon")
+                }
+            }
+
+            route("overlay") {
+                getOverlay()
+            }
+        }
     }
 }
 
