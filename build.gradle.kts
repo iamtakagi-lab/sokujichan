0633163d58cb4e9311e8e3be795e7a1b8e0e2fae
takagi <iam.takagi.dev@gmail.com>
2021-02-10 00:22:40 +0900

    fix: バージョン管理を削除

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,19 @@
 group = "me.takagi.sokujichan"
-val artifactID = "sokujichan"
-version = Version(1, 0, 3).label
-data class Version(val major: Int, val minor: Int, val patch: Int) {
-    val label: String
-        get() = "$major.$minor.$patch"
-}
 
 object ThirdpartyVersion {
     const val Ktor = "1.5.1"
     const val KotlinCssJvm = "1.0.0-pre.86-kotlin-1.3.50"
-    const val Logback = "1.2.3"
 
     const val JDA = "4.2.0_168"
     const val JDAUtilities = "3.0.5"
 
+    const val KotlinLogging = "2.0.4"
+    const val Logback = "1.2.3"
+    const val jansi = "1.18"
+
     const val CommonsLang = "3.3.1"
+
+    const val JUnit = "5.7.0"
 }
 
 plugins {
@@ -30,20 +29,38 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${ThirdpartyVersion.Ktor}")
+    // Ktor Server
     implementation("io.ktor:ktor-server-cio:${ThirdpartyVersion.Ktor}")
-    implementation("ch.qos.logback:logback-classic:${ThirdpartyVersion.Ktor}")
     implementation( "io.ktor:ktor-server-core:${ThirdpartyVersion.Ktor}")
-    testImplementation("io.ktor:ktor-server-tests:${ThirdpartyVersion.Ktor}")
-    implementation( "io.ktor:ktor-jackson:${ThirdpartyVersion.Ktor}")
+
+    // HTML
     implementation( "io.ktor:ktor-html-builder:${ThirdpartyVersion.Ktor}")
     implementation( "org.jetbrains:kotlin-css-jvm:${ThirdpartyVersion.KotlinCssJvm}")
 
+    // JDA
     implementation("net.dv8tion:JDA:${ThirdpartyVersion.JDA}")
     implementation("com.jagrosh:jda-utilities:${ThirdpartyVersion.JDAUtilities}")
-    implementation("org.apache.commons:commons-lang3:${ThirdpartyVersion.CommonsLang}")
-}
 
+    // Logging
+    implementation("io.github.microutils:kotlin-logging:${ThirdpartyVersion.KotlinLogging}")
+    implementation("ch.qos.logback:logback-core:${ThirdpartyVersion.Logback}")
+    implementation("ch.qos.logback:logback-classic:${ThirdpartyVersion.Logback}")
+    implementation("org.fusesource.jansi:jansi:${ThirdpartyVersion.jansi}")
+
+    // Util
+    implementation("org.apache.commons:commons-lang3:${ThirdpartyVersion.CommonsLang}")
+
+    // Logging
+    implementation("io.github.microutils:kotlin-logging:${ThirdpartyVersion.KotlinLogging}")
+    implementation("ch.qos.logback:logback-core:${ThirdpartyVersion.Logback}")
+    implementation("ch.qos.logback:logback-classic:${ThirdpartyVersion.Logback}")
+    implementation("org.fusesource.jansi:jansi:${ThirdpartyVersion.jansi}")
+
+    // Testing
+    testImplementation(kotlin("test"))
+    testImplementation(kotlin("test-junit5"))
+    testImplementation("org.junit.jupiter:junit-jupiter:${ThirdpartyVersion.JUnit}")
+}
 
 kotlin {
     target {
@@ -67,6 +84,7 @@ kotlin {
         }
     }
 }
+
 
 tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
     manifest {
